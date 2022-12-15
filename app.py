49f66d5f43d4fcf2900ce7b455a8316cf9792a1a
Nagarjun <99671332+arjunc2@users.noreply.github.com>
2022-12-15 12:03:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Please subscribe, like, and comment on this demo, TY!!!'
+    return 'This is GitOps demo executed successfully..Thanks to Raj!!'
